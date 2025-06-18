97d87ca6e6a8cfd388cbae00895ff582f880e54c
Steve <stevedsimkins@gmail.com>
2025-06-18 10:31:20 -0400

    chore: Updated reo

--- reo.js
+++ reo.js
@@ -1,3 +1,14 @@
-    <script type="text/javascript">
-      !function(){var e,t,n;e="3ca5238beb530cd",t=function(){Reo.init({clientID:"3ca5238beb530cd"})},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.defer=!0,n.onload=t,document.head.appendChild(n)}()
-    </script>
+function reo() {
+	var e, t, n;
+	e = "3ca5238beb530cd";
+	t = () => {
+		Reo.init({ clientID: "3ca5238beb530cd" });
+	};
+	n = document.createElement("script");
+	n.src = `https://static.reo.dev/${e}/reo.js`;
+	n.defer = true;
+	n.onload = t;
+	document.head.appendChild(n);
+}
+
+reo();
